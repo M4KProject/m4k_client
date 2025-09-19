a468b63090b004a73d2a9129820b9e431094d07e
Benoit ADAM <b.adam.info@gmail.com>
2025-09-19 17:32:00 +0200

    MediaPreview variants video

--- src/admin/components/MediaPreview.tsx
+++ src/admin/components/MediaPreview.tsx
@@ -2,7 +2,9 @@ import { Css } from '@common/ui';
 import { useCss } from '@common/hooks';
 import { Div } from '@common/components';
 import { collMedias } from '@common/api/collMedias';
-import { MediaModel } from '@common/api/models';
+import { FileInfo, MediaModel } from '@common/api/models';
+import { useState } from 'preact/hooks';
+import { first, isStrNotEmpty } from '@common/utils';
 
 const css: Css = {
   '&': {
@@ -13,7 +15,6 @@ const css: Css = {
     bgMode: 'contain',
     transition: 0.2,
     userSelect: 'none',
-    pointerEvents: 'none',
   },
   '&:hover': {
     xy: '50%',
@@ -25,17 +26,67 @@ const css: Css = {
     // borderWidth: '0.5em',
     rounded: 1,
   },
+  '& video': {
+    wh: '100%',
+  }
 };
+
+interface Variant extends FileInfo {
+  media: MediaModel;
+  file: string;
+}
+
+const getVariants = (media?: MediaModel): Variant[] => {
+  if (!media || !media.data) return [];
+  const results = [];
+  media.data.variants.forEach((info, i) => {
+    const file = media.variants[i];
+    if (isStrNotEmpty(file)) {
+      results.push({ ...info, file, media });
+    }
+  });
+  if (isStrNotEmpty(media.source)) {
+    const file = media.source;
+    results.push({ ...media, file, media });
+  }
+  return results;
+}
+
+const getThumbUrl = (v?: Variant) => v && collMedias.getThumbUrl(v.media.id, v.file, [300, 300]) || '';
+const getUrl = (v?: Variant) => v && collMedias.getUrl(v.media.id, v.file) || '';
 
 export const MediaPreview = ({ media }: { media: MediaModel }) => {
   const c = useCss('MediaPreview', css);
 
+  const variants = getVariants(media);
+
+  const images = variants.filter(v => v.type === 'image');
+  const videos = variants.filter(v => v.type === 'video');
+
+  const [isOver, setIsOver] = useState(false);
+  
   return (
     <Div
       cls={c}
-      style={{
-        backgroundImage: `url("${collMedias.getThumbUrl(media.id, media.source, [300, 300])}")`,
+      onMouseOver={() => {
+        console.debug('onMouseOver', media.id);
+        setIsOver(true);
       }}
-    />
+      onMouseLeave={() => {
+        console.debug('onMouseLeave', media.id);
+        setIsOver(false);
+      }}
+      style={{
+        backgroundImage: `url("${getThumbUrl(images[0])}")`,
+      }}
+    >
+      {(isOver && videos.length) ? (
+        <video controls muted autoPlay>
+          {videos.map(v => (
+            <source type={v.mime} src={getUrl(v)} />
+          ))}
+        </video>
+      ) : null}
+    </Div>
   );
 };
