344d241f77a34626ae0a57260f94953889aba755
Benoit ADAM <b.adam.info@gmail.com>
2025-09-23 12:52:57 +0200

    useQuery

--- src/admin/pages/PlaylistsPage.tsx
+++ src/admin/pages/PlaylistsPage.tsx
@@ -1,10 +1,16 @@
 import { Css } from '@common/ui';
 import { Page, PageBody } from '@common/components';
 import { MediaTable } from '../components/MediaTable';
+import { useMsg } from '@common/hooks';
+import { mediaId$ } from '../messages';
+import { mediaCtrl } from '@/colls';
 
 const c = Css('PlaylistsPage', {});
 
 export const PlaylistsPage = () => {
+  const id = useMsg(mediaId$);
+  const playlist = useMsg(mediaCtrl.one$({ id, type: 'playlist' }));
+
   return (
     <Page class={c('Page')}>
       <PageBody>
