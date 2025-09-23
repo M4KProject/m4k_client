2636470f83ef993af770196913e92f6fba654500
Benoit ADAM <b.adam.info@gmail.com>
2025-09-23 14:44:12 +0200

    newColors

--- src/admin/pages/AccountPage.tsx
+++ src/admin/pages/AccountPage.tsx
@@ -1,6 +1,6 @@
-import { Css, refreshTheme, setTheme, theme$ } from '@common/ui';
+import { Css, getColor, refreshTheme, setTheme, theme$ } from '@common/ui';
 import { RefreshCw } from 'lucide-react';
-import { Page, PageBody, PageActions, Toolbar } from '@common/components';
+import { Page, PageBody, PageActions, Toolbar, FieldGroup } from '@common/components';
 import { Button } from '@common/components';
 import { Form } from '@common/components';
 import { Field } from '@common/components';
@@ -11,7 +11,18 @@ import { LoadingPage } from './LoadingPage';
 import { isAdvanced$ } from '../messages';
 import { logout, auth$, coll } from '@common/api';
 
-const c = Css('AccountPage', {});
+const c = Css('AccountPage', {
+  Color: {
+    wh: 2,
+    bg: 'red',
+  },
+});
+
+export const Color = ({ color } : { color: string }) => {
+  return (
+    <div class={c('Color')} style={{ background: getColor(color) }} />
+  )
+}
 
 export const AccountPage = () => {
   const auth = useMsg(auth$);
@@ -66,16 +77,72 @@ export const AccountPage = () => {
               refreshTheme();
             }}
           />
-          <Field
-            type="color"
-            label="Couleur de l'interface"
-            name="primary"
-            value={theme.primary}
-            onValue={primary => {
-              setTheme({ primary })
-              refreshTheme();
-            }}
-          />
+          <FieldGroup>
+            <Field
+              type="color"
+              label="Couleur primaire"
+              name="primary"
+              value={theme.primary}
+              onValue={primary => {
+                setTheme({ primary })
+                refreshTheme();
+              }}
+            />
+            <Color color={'#1b1b1b'} />
+            <Color color={'#3b3b3b'} />
+            <Color color={'#5a5a5a'} />
+            <Color color={'#6d6d6d'} />
+            <Color color={'#969696'} />
+            <Color color={'#b6b6b6'} />
+            <Color color={'#d9d9d9'} />
+            <Color color={'#e9e9e9'} />
+            <Color color={'#f2f2f2'} />
+            <Color color={'#f9f9f9'} />
+          </FieldGroup>
+          <FieldGroup>
+            <Field
+              type="color"
+              label="Couleur primaire"
+              name="primary"
+              value={theme.primary}
+              onValue={primary => {
+                setTheme({ primary })
+                refreshTheme();
+              }}
+            />
+            <Color color={'p90'} />
+            <Color color={'p80'} />
+            <Color color={'p70'} />
+            <Color color={'p60'} />
+            <Color color={'p50'} />
+            <Color color={'p40'} />
+            <Color color={'p30'} />
+            <Color color={'p20'} />
+            <Color color={'p10'} />
+            <Color color={'p5'} />
+          </FieldGroup>
+          <FieldGroup>
+            <Field
+              type="color"
+              label="Couleur secondaire"
+              name="primary"
+              value={theme.secondary}
+              onValue={secondary => {
+                setTheme({ secondary })
+                refreshTheme();
+              }}
+            />
+            <Color color={'s90'} />
+            <Color color={'s80'} />
+            <Color color={'s70'} />
+            <Color color={'s60'} />
+            <Color color={'s50'} />
+            <Color color={'s40'} />
+            <Color color={'s30'} />
+            <Color color={'s20'} />
+            <Color color={'s10'} />
+            <Color color={'s5'} />
+          </FieldGroup>
           <Field
             label="Ancien mot de passe"
             name="current-password"
