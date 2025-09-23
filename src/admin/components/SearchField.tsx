29c2c38cb18d55d23f8dd08ccf8a04c9df44557f
Benoit ADAM <b.adam.info@gmail.com>
2025-09-23 21:13:47 +0200

    SearchField small

--- src/admin/components/SearchField.tsx
+++ src/admin/components/SearchField.tsx
@@ -1,11 +1,10 @@
 import { Field } from '@common/components';
 import { useMsg } from '@common/hooks';
-
 import { Search } from 'lucide-react';
 import { Css } from '@common/ui';
-
 import { Button } from '@common/components';
 import { search$ } from '../messages/search$';
+import { Msg } from '@common/utils';
 
 const c = Css('SearchField', {
   '': {
@@ -13,13 +12,16 @@ const c = Css('SearchField', {
   },
 });
 
+const isSearchOpen$ = new Msg(false);
+
 export const SearchField = () => {
   const search = useMsg(search$);
+  const isSearchOpen = useMsg(isSearchOpen$);
 
   return (
     <div class={c()}>
-      <Field name="search" value={search} onValue={search$.setter()} />
-      <Button icon={<Search />} color="primary" onClick={() => search$.signal()} />
+      {isSearchOpen && <Field name="search" value={search} onValue={search$.setter()} />}
+      <Button icon={<Search />} color="primary" onClick={() => isSearchOpen$.next(prev => !prev)} />
     </div>
   );
 };
