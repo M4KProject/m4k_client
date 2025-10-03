810d21cffee78af3290eff44b4d44e28c4e8564b
Benoit ADAM <b.adam.info@gmail.com>
2025-10-03 23:04:44 +0200

    auto

--- src/components/PageView.tsx
+++ src/components/PageView.tsx
@@ -1,27 +1,75 @@
-import { Css } from '@common/ui';
-import { MediaModel } from '@common/api';
+import { Css, addJsFile, addCssFile } from '@common/ui';
+import { setCss } from '@common/ui/css';
+import { PageModel } from '@common/api';
 import { DivProps } from '@common/components';
-import { MediaConfig } from './MediaView';
+import { useEffect } from 'preact/hooks';
+import { useMedias } from '@/api/hooks';
+import { MediaConfig, MediaView } from './MediaView';
 
 const c = Css('PageView', {
   '': {
     position: 'absolute',
     overflow: 'hidden',
-    fCenter: 1,
     wh: '100%',
     xy: 0,
+  },
+  HtmlContent: {
+    position: 'absolute',
+    inset: 0,
+    overflow: 'auto',
+  },
+  MediaItem: {
+    position: 'absolute',
   },
 });
 
 export type PageViewProps = DivProps &
   MediaConfig & {
-    media?: MediaModel;
+    media?: PageModel;
   };
 
-export const PageView = (props: PageViewProps) => {
+export const PageView = ({ media, ...props }: PageViewProps) => {
+  const allMedias = useMedias();
+  const data = media?.data || {};
+  const { items = [], html, css, jsUrls = [], cssUrls = [] } = data;
+
+  // Load external resources
+  useEffect(() => {
+    if (css) setCss('PageView-custom', css);
+    jsUrls.forEach((url: string) => addJsFile(url));
+    cssUrls.forEach((url: string) => addCssFile(url));
+  }, [css, jsUrls, cssUrls]);
+
+  // Helper to find media by ID
+  const getMediaById = (id: string) => allMedias.find((m) => m.id === id);
+
   return (
     <div {...props} class={c(props)}>
-      TODO
+      {/* HTML content */}
+      {html && <div class={c('HtmlContent')} dangerouslySetInnerHTML={{ __html: html }} />}
+
+      {/* Positioned media items */}
+      {items.map((item: any, index: number) => {
+        const { x = 0, y = 0, w = 100, h = 100, media: mediaId } = item;
+        const mediaItem = getMediaById(mediaId);
+
+        if (!mediaItem) return null;
+
+        return (
+          <div
+            key={index}
+            class={c('MediaItem')}
+            style={{
+              left: `${x}%`,
+              top: `${y}%`,
+              width: `${w}%`,
+              height: `${h}%`,
+            }}
+          >
+            <MediaView media={mediaItem} {...props} />
+          </div>
+        );
+      })}
     </div>
   );
 };
