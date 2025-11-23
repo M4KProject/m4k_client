91efa714df6da57906214e93c0754d2a7c8a7e80
BenoitAdam <b.adam.info@gmail.com>
2025-11-24 00:49:29 +0100

    u

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,8 @@ import { glb } from 'fluxio';
 
 import * as fluxio from 'fluxio';
 import * as pb from 'pblite';
-import * as router from './router';
 import { bridge } from './bridge';
 
 export const app = glb.m4k || (glb.m4k = {});
 
-Object.assign(app, fluxio, pb, router, bridge);
+Object.assign(app, fluxio, pb, bridge);
