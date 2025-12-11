41705f239236c9f088a9cc0f364908fce0265272
BenoitAdam <b.adam.info@gmail.com>
2025-12-11 01:26:33 +0100

    u

--- src/components/box/edit/side/BSideFilter.tsx
+++ src/components/box/edit/side/BSideFilter.tsx
@@ -1,61 +1,12 @@
-import { bitsToArray, Css, dayIndexToShort, firstUpper, getBit, repeat, setBit, toArray, toFalse, VECTOR3_ZERO } from 'fluxio';
+import { bitsToRecord, Css, dayIndexToShort, recordToBits } from 'fluxio';
 import { useProp } from '../BField';
-import { Button, ButtonProps } from '@/components/common/Button';
 import { Field } from '@/components/fields/Field';
 import { BItem } from '@/components/box/bTypes';
 import { BCalendar } from '../../../common/BCalendar';
 import { useApi } from '@/hooks/useApi';
-import { useBEditController } from '../useBEditController';
 
 const c = Css('BSideFilter', {
-  '': {
-    col: ['stretch', 'start'],
-    gap: 2,
-  },
-  DateRow: {
-    row: 1,
-    gap: 2,
-  },
-  Day: {
-    flex: 1,
-    h: 38,
-    m: 0,
-    ml: -1,
-    p: 0,
-    border: 'border',
-    center: 1,
-    rounded: 0,
-  },
-  'Day:first-child': { rounded: [5, 0, 0, 5] },
-  'Day:last-child': { rounded: [0, 5, 5, 0] },
-  'Day .ButtonContent': {
-    rotate: '-45deg',
-    fontSize: '80%',
-  },
 });
-
-const DayButton = ({ day, ...props}: ButtonProps & { day: number }) => {
-  const controller = useBEditController()!;
-  const [bits, setBits] = useFilterProp('w');
-  const selected = getBit(bits||0, day);
-  console.debug('DayButton', bits, day, selected);
-  return (
-    <Button
-      title={firstUpper(dayIndexToShort(day))}
-      selected={selected}
-      onClick={() => {
-        const prevBits = bits||0;
-        const prevItem = getBit(prevBits, day);
-        const nextItem = !prevItem;
-        const nextBits = setBit(prevBits, day, nextItem);
-        console.debug('day setBits', day, prevBits, prevItem, nextItem, nextBits);
-        setBits(nextBits);
-      }}
-      {...props}
-      {...c('Day', props)}
-    />
-  )
-};
 
 const useFilterProp = <K extends keyof NonNullable<BItem['f']>>(
   prop: K
@@ -68,6 +19,22 @@ const useFilterProp = <K extends keyof NonNullable<BItem['f']>>(
   return [value, setValue];
 };
 
+const WeekDaysField = () => {
+  const [dates, setDates] = useFilterProp('w');
+  const value = bitsToRecord(dates||0);
+
+  return (
+    <Field
+      label="Jours de la semaine"
+      col
+      type="picker"
+      items={[1,2,3,4,5,6,0].map(v => [v as any, dayIndexToShort(v)])}
+      value={value}
+      onValue={next => setDates(recordToBits(next))}
+    />
+  )
+}
+
 export const BSideFilter = () => {
   const api = useApi();
   const [dates, setDates] = useFilterProp('d');
@@ -76,49 +43,12 @@ export const BSideFilter = () => {
   const dateRange = dates?.[0] || ['', ''];
   const hourRange = hours?.[0] || [0, 24];
 
-  // const toggleDay = (dayIndex: number) => {
-  //   const current = weekDays || [false, false, false, false, false, false, false];
-  //   const next = [...current];
-  //   next[dayIndex] = !current[dayIndex];
-  //   setWeekDays(next as any);
-  // };
-
   return (
     <div {...c('')}>
       <Field label="Plage de dates" col>
         <BCalendar />
       </Field>
-
-      {/* <Field label="Plage horaire" col>
-        <div {...c('DateRow')}>
-          <Field
-            label="Début"
-            type="number"
-            min={0}
-            max={24}
-            value={hourRange[0]}
-            onValue={(v) => setHours([[v, hourRange[1]]])}
-          />
-          <Field
-            label="Fin"
-            type="number"
-            min={0}
-            max={24}
-            value={hourRange[1]}
-            onValue={(v) => setHours([[hourRange[0], v]])}
-          />
-        </div>
-      </Field> */}
-
-      <Field label="Jours de la semaine" col>
-        <DayButton day={1} />
-        <DayButton day={2} />
-        <DayButton day={3} />
-        <DayButton day={4} />
-        <DayButton day={5} />
-        <DayButton day={6} />
-        <DayButton day={0} />
-      </Field>
+      <WeekDaysField />
     </div>
   );
 };
--- src/components/fields/inputs/picker.tsx
+++ src/components/fields/inputs/picker.tsx
@@ -1,5 +1,5 @@
 import { ComponentChildren } from 'preact';
-import { Css } from 'fluxio';
+import { Css, Dictionary, isDictionary } from 'fluxio';
 import { Button } from '@/components/common/Button';
 import { isArray } from 'fluxio';
 import { useFieldController, useFieldState } from '@/components/fields/hooks';
@@ -7,21 +7,41 @@ import { FieldProps } from '@/components/fields/types';
 
 const c = Css('Picker', {
   '': {
-    row: ['center', 'center'],
-    flexWrap: 'wrap',
     w: '100%',
-    p: 0,
-    border: 0,
+    rowWrap: 1,
   },
   ' .Button': {
+    flex: 1,
+    h: 38,
     m: 0,
     p: 0,
-    hMin: '1em',
+    border: 'border',
+    br: 0,
+    center: 1,
+    rounded: 0,
   },
-  ' .Button .ButtonIcon': {
-    wh: '1em',
-    bg: 'transparent',
+  ' .Button:first-child': { rounded: [5, 0, 0, 5] },
+  ' .Button:last-child': { rounded: [0, 5, 5, 0], br: 1 },
+  ' .ButtonContent': {
+    rotate: '-45deg',
+    fontSize: '80%',
   },
+  // '': {
+  //   row: ['center', 'center'],
+  //   flexWrap: 'wrap',
+  //   w: '100%',
+  //   p: 0,
+  //   border: 0,
+  // },
+  // ' .Button': {
+  //   m: 0,
+  //   p: 0,
+  //   hMin: '1em',
+  // },
+  // ' .Button .ButtonIcon': {
+  //   wh: '1em',
+  //   bg: 'transparent',
+  // },
 });
 
 interface PickerProps {
@@ -34,55 +54,90 @@ interface PickerProps {
   props?: any;
 }
 
-const Picker = ({ name, required, value = '', onChange, items = [], ...props }: PickerProps) => {
-  const validItems = items.filter((item) => isArray(item)) as [string, ComponentChildren][];
+// const Picker = ({
+//   name,
+//   required,
+//   value = '',
+//   onChange,
+//   items = [],
+//   ...props
+// }: PickerProps) => {
+//   const handleIconClick = (iconValue: string) => {
+//     onChange?.(iconValue);
+//   };
 
-  const handleIconClick = (iconValue: string) => {
-    onChange?.(iconValue);
-  };
+//   return (
+//     <div {...props} {...c('', props)}>
+//       {/* Hidden input for form compatibility */}
+//       <input
+//         name={name}
+//         required={required}
+//         value={value}
+//         style={{ display: 'none' }}
+//         tabIndex={-1}
+//       />
+
+//       {validItems.map(([key, icon]) => (
+//         <Button
+//           key={key}
+//           icon={icon}
+//           selected={key === value}
+//           color={key === value ? 'primary' : undefined}
+//           onClick={() => handleIconClick(key)}
+//           // title={key} // Tooltip showing the key/value
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
+// const PickerInput = () => {
+//   const ctrl = useFieldController();
+//   const { config, value } = useFieldState(ctrl, 'config', 'value');
+
+//   return (
+//     <Picker
+//       items={config.items}
+//       value={value}
+//       onChange={ctrl.onChange}
+//       name={config.name}
+//       required={config.required}
+//     />
+//   );
+// };
+
+const Picker = () => {
+  const controller = useFieldController();
+  const { config, raw } = useFieldState(controller, 'config', 'raw');
+  const props = config.props;
+  const values: Dictionary<1> = isDictionary(raw) ? raw : {};
+
+  const isSelected = (index: any) => !!values[index];
+
+  const getHandle = (index: any) => () => {
+    const next = {...values};
+    if (next[index]) delete next[index];
+    else next[index] = 1;
+    controller.update({ raw: next });
+  }
 
   return (
     <div {...props} {...c('', props)}>
-      {/* Hidden input for form compatibility */}
-      <input
-        name={name}
-        required={required}
-        value={value}
-        style={{ display: 'none' }}
-        tabIndex={-1}
-      />
-
-      {validItems.map(([key, icon]) => (
+      {config.items?.map((item) => isArray(item) ? (
         <Button
-          key={key}
-          icon={icon}
-          selected={key === value}
-          color={key === value ? 'primary' : undefined}
-          onClick={() => handleIconClick(key)}
-          // title={key} // Tooltip showing the key/value
-        />
-      ))}
+          key={item[0]}
+          selected={isSelected(item[0])}
+          onClick={getHandle(item[0])}
+        >
+          {item[1]}
+        </Button>
+      ) : null)}
     </div>
   );
 };
 
-const PickerInput = () => {
-  const ctrl = useFieldController();
-  const { config, value } = useFieldState(ctrl, 'config', 'value');
-
-  return (
-    <Picker
-      items={config.items}
-      value={value}
-      onChange={ctrl.onChange}
-      name={config.name}
-      required={config.required}
-    />
-  );
-};
-
 const picker: FieldProps<any, string> = {
-  input: PickerInput,
+  input: Picker,
   delay: 10,
 };
 
